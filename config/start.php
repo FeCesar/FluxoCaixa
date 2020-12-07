de4e528682e881b88a2b59bb7f78a8b192003521
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-12-07 19:56:27 -0300

    Criação do arquivo criador do banco de dados e das tabelas do sistema

---
+++ config/start.php
@@ -0,0 +1,28 @@
+<?php
+
+    try{
+        $conn = new PDO('mysql:host=localhost', 'root', '');
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "create database if not exists estacionamentodb";
+        $conn->exec($sql);
+        $sql = "use estacionamentodb";
+        $conn->exec($sql);
+        $sql = "create table if not exists configs(
+            configs_id int primary key auto_increment,
+            configs_valor float
+        )";
+        $conn->exec($sql);
+        $sql = "create table if not exists carros(
+            carro_id int primary key auto_increment,
+            carro_placa varchar(7),
+            carro_horario_entrada time,
+            carro_horario_saida time,
+            carro_dia_entrada date
+        )";
+        $conn->exec($sql);
+        
+    } catch (PDOException $e){
+        echo $sql . "<br>" . $e->getMessage();
+    }
+
+?>
